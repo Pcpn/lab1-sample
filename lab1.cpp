3d57b3c3508028088c379da4c11f0a009e2242a7
Pcpn <42600610+Pcpn@users.noreply.github.com>
2018-08-23 20:13:29 +0530

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,11 @@
+#include<iostream> // library
+using namespace std;
+// resovle the multiple defination issue of same name functions;
+int main(){
+cout <<"Hello";
+//print out of terminal
+return 10 ;
+}
+
+
+
